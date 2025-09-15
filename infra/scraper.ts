ebc1b8a587fa9b08b005f43e3de7e4a54345830c
itspauv <p.viehmann01@gmail.com>
2025-09-15 20:32:47 +0200

    feat: call extraction worker from scraper worker

--- infra/scraper.ts
+++ infra/scraper.ts
@@ -1,4 +1,10 @@
+import { recipeExtraction } from "./recipe-extraction";
+
 export const scraper = new sst.cloudflare.Worker("Scraper", {
   handler: "packages/functions/src/scraper.ts",
   url: true,
+  link: [recipeExtraction],
+  environment: {
+    RECIPE_EXTRACTION_URL: recipeExtraction.url,
+  },
 });
--- packages/functions/src/scraper.ts
+++ packages/functions/src/scraper.ts
@@ -1,15 +1,26 @@
 import { Scraper } from "@kochbuch/core/scraper";
+import { Resource } from "sst";
 
 export default {
-  async fetch(req: Request) {
+  async fetch(req: Request, env: { RECIPE_EXTRACTION_URL: string }) {
     if (req.method == "GET") {
       const url = new URL(req.url).searchParams.get("url");
       if (!url) {
         return new Response("Missing url parameter", { status: 400 });
       }
 
-      const result = await Scraper.scrape(url);
-      return Response.json(result);
+      const { textContent } = await Scraper.scrape(url);
+
+      const response = await Resource.RecipeExtraction.fetch(
+        env.RECIPE_EXTRACTION_URL,
+        {
+          method: "POST",
+          body: JSON.stringify({ textContent }),
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+
+      return response;
     }
 
     return new Response("Method not allowed", { status: 405 });
