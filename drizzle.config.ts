b1aa0509149c6abdd6a14f81c32f65533486d387
itspauv <p.viehmann01@gmail.com>
2025-10-02 21:09:48 +0200

    feat: make drizzle kit work in dev

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import { Resource } from "sst";
+
+export default defineConfig({
+  dialect: "sqlite",
+  driver: "d1-http",
+  schema: "./packages/core/src/db/schema.ts",
+  out: "./packages/core/src/db/migrations",
+  dbCredentials: {
+    databaseId: Resource.DB.databaseId,
+    accountId: process.env.CLOUDFLARE_DEFAULT_ACCOUNT_ID,
+    token: process.env.CLOUDFLARE_API_TOKEN,
+  },
+});
